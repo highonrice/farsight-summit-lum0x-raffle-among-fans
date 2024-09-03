b20d74fb488da5cfb7e95f74e9fddf1e46dde165
ella-aaa <ells@sooho.io>
2024-09-03 14:54:29 +0900

    안쓰는 로직 제거

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -5,40 +5,13 @@ import { devtools } from "frog/dev";
 import { neynar } from "frog/hubs";
 import { handle } from "frog/next";
 import { serveStatic } from "frog/serve-static";
-import path from "path";
-import fs from "fs/promises";
-import { Fan, Participant } from "../../utils/interface";
+import { Participant } from "../../utils/interface";
 import {
   getDisplayName,
-  getTop10Fans,
   getUserPfpUrl,
-  weightedRaffle,
-  postLum0xTestFrameValidation,
   getUserFromChannel,
 } from "../../utils/helpers";
-// import { createFan, getFan } from "@/app/utils/db/queries/fans";
-import {
-  createParticipant,
-  getParticipant,
-} from "@/app/utils/db/queries/participants";
-import { getLeaderboardImage } from "../../ui/leaderboard";
 import { getShareImage } from "../../ui/share";
-
-async function getRegularFontData() {
-  const fontPath = path.resolve(
-    "./public/fonts/Coinbase_Display-Regular-web-1.32.ttf"
-  );
-  const fontData = await fs.readFile(fontPath);
-  return fontData;
-}
-
-async function getBoldFontData() {
-  const fontPath = path.resolve(
-    "./public/fonts/Coinbase_Display-Bold-web-1.32.ttf"
-  );
-  const fontData = await fs.readFile(fontPath);
-  return fontData;
-}
 
 const app = new Frog({
   assetsPath: "/",
@@ -49,142 +22,23 @@ const app = new Frog({
   imageOptions: {
     height: 1106,
     width: 1106,
-    fonts: [
-      {
-        name: "coinbase",
-        data: await getRegularFontData(),
-        style: "normal",
-        weight: 400,
-      },
-      {
-        name: "coinbase",
-        data: await getBoldFontData(),
-        style: "normal",
-        weight: 700,
-      },
-    ],
   },
 });
-
-// Uncomment to use Edge Runtime
-// export const runtime = 'edge'
 
 app.frame("/", (c) => {
   return c.res({
     image: "/Default.png",
-    intents: [
-      <Button action="/result">Raffle!</Button>,
-      // <Button action="/fans">My Top 10 Fans</Button>,
-    ],
-  });
-});
-
-app.frame("/raffle", async (c) => {
-  const fid = c.frameData?.fid;
-
-  await postLum0xTestFrameValidation(Number(fid), "raffle");
-
-  // const winner: Fan = await weightedRaffle(Number(fid));
-
-  // const winner: Participant = await getUserFromChannel();
-  // console.log(winner);
-  // const fanDocRef = await createFan(winner);
-  // const participantDocRef = await createParticipant(winner);
-
-  // const frameUrl = `${process.env.BASE_URL}/api/share/${fanDocRef.id}`;
-  // const frameUrl = `${process.env.BASE_URL}/api/share/${participantDocRef.id}`;
-  const message = `Winner of the raffle is...!!`;
-  const urlMessage = message.replace(/ /g, "%20");
-  // const shareUrl = `https://warpcast.com/~/compose?text=${urlMessage}&embeds[]=${frameUrl}`;
-
-  return c.res({
-    image: "/Result.png",
-    intents: [
-      // <Button.Link href={shareUrl}>Share</Button.Link>,
-      <Button action="/result/">Share</Button>,
-      <Button action="/">Back</Button>,
-    ],
+    intents: [<Button action="/result">Raffle!</Button>],
   });
 });
 
 app.frame("/result", async (c) => {
   const winner: Participant = await getUserFromChannel();
-
-  // const participantDoc = await getParticipant(String(winner.fid));
-  // const { fid } = participantDoc.data() as Participant;
   const displayName = await getDisplayName(String(winner.fid));
   const pfpUrl = await getUserPfpUrl(winner.fid);
   return c.res({
-    image: getShareImage(
-      // ranks,
-      // score,
-      // reactions,
-      // recasts,
-      displayName[0],
-      pfpUrl
-    ),
-    intents: [
-      // <Button action="/raffle">Raffle</Button>,
-      <Button action="/">Back</Button>,
-    ],
-  });
-});
-
-app.frame("/fans", async (c) => {
-  const fid = c.frameData?.fid;
-  if (!fid) {
-    return c.error({ message: "Invalid fid", statusCode: 404 });
-  }
-
-  await postLum0xTestFrameValidation(Number(fid), "fans");
-
-  return c.res({
-    image: `/leaderboard-image/${fid}`,
-    intents: [
-      <Button action="/raffle">Raffle</Button>,
-      <Button action="/">Back</Button>,
-    ],
-  });
-});
-
-app.frame("/share/:id", async (c) => {
-  const viewerFid = c.frameData?.fid;
-  const id = c.req.param("id");
-  // const fanDoc = await getFan(id);
-  const participantDoc = await getParticipant(id);
-
-  await postLum0xTestFrameValidation(Number(viewerFid), "share");
-
-  // const { fid, ranks, score, reactions, recasts } = fanDoc.data() as Fan;
-  const { fid } = participantDoc.data() as Participant;
-  const displayName = await getDisplayName(String(fid));
-  const pfpUrl = await getUserPfpUrl(fid);
-  return c.res({
-    image: getShareImage(
-      // ranks,
-      // score,
-      // reactions,
-      // recasts,
-      displayName[0],
-      pfpUrl
-    ),
-    intents: [
-      <Button action="/raffle">Raffle</Button>,
-      // <Button action="/fans">My Top 10 Fans</Button>,
-    ],
-  });
-});
-
-app.image("/leaderboard-image/:fid", async (c) => {
-  const fid = c.req.param("fid");
-  if (fid == undefined) {
-    return c.res({
-      image: <img src="/Error.png" />,
-    });
-  }
-  const top10Fans: Fan[] = await getTop10Fans(Number(fid));
-  return c.res({
-    image: getLeaderboardImage(top10Fans),
+    image: getShareImage(displayName[0], pfpUrl),
+    intents: [<Button action="/">Back</Button>],
   });
 });
 
--- app/utils/helpers.ts
+++ app/utils/helpers.ts
@@ -1,126 +1,13 @@
 import { Lum0x } from "lum0x-sdk";
-import { Cast, Fan, Participant } from "./interface";
+import { Participant } from "./interface";
 
 Lum0x.init(process.env.LUM0X_API_KEY || "");
-
-let scoreBoard: Record<number, Fan> = {};
-
-const reactionScore = 1;
-const recastScore = 3;
-
-// Sorts the scores of users in descending order.
-function getSortedScores(): Fan[] {
-  const sortedScores: Fan[] = Object.values(scoreBoard);
-
-  sortedScores.sort((a, b) => b.score - a.score);
-
-  for (let i = 0; i < sortedScores.length; i++) {
-    scoreBoard[sortedScores[i].fid].ranks = i + 1;
-  }
-
-  return sortedScores;
-}
-
-// Updates the scoreBoard based on reactions and recasts for a given Cast.
-// function processReaction(cast: Cast): void {
-//   const { reactions, recasts } = cast;
-//   const reactionFids = reactions.fids;
-//   const recastFids = recasts.fids;
-
-//   for (const reactionFid of reactionFids) {
-//     if (!scoreBoard[reactionFid]) {
-//       scoreBoard[reactionFid] = {
-//         fid: reactionFid,
-//         score: 0,
-//         reactions: 0,
-//         recasts: 0,
-//         ranks: 0,
-//       };
-//     }
-//     scoreBoard[reactionFid].score += reactionScore;
-//     scoreBoard[reactionFid].reactions += 1;
-//   }
-
-//   for (const recastFid of recastFids) {
-//     if (!scoreBoard[recastFid]) {
-//       scoreBoard[recastFid] = {
-//         fid: recastFid,
-//         score: 0,
-//         reactions: 0,
-//         recasts: 0,
-//         ranks: 0,
-//       };
-//     }
-//     scoreBoard[recastFid].score += recastScore;
-//     scoreBoard[recastFid].recasts += 1;
-//   }
-// }
 
 export async function getDisplayName(fids: string): Promise<string[]> {
   const res = await Lum0x.farcasterUser.getUserByFids({ fids: fids });
   // const users: User[] = res.users;
   const participants: Participant[] = res.users;
   return participants.map((participant) => participant.display_name);
-}
-
-async function searchMyFan(fid: number): Promise<Fan[]> {
-  const res = await Lum0x.farcasterCast.getCastsByFid({
-    fid: fid,
-    limit: 100,
-  });
-
-  const casts: Cast[] = res.result.casts;
-  for (const cast of casts) {
-    // processReaction(cast);
-  }
-
-  const sortedFids: Fan[] = getSortedScores();
-
-  return sortedFids;
-}
-
-export async function getTop10Fans(fid: number): Promise<Fan[]> {
-  const res = await Lum0x.farcasterCast.getCastsByFid({
-    fid: fid,
-    limit: 100,
-  });
-
-  const casts: Cast[] = res.result.casts;
-  for (const cast of casts) {
-    // processReaction(cast);
-  }
-
-  const sortedFids: Fan[] = getSortedScores();
-
-  // Limit to top 10 scores
-  sortedFids.splice(10);
-
-  // Create a comma-separated string of FIDs
-  let fids = sortedFids.map((info) => info.fid).join(",");
-
-  const displayNames: string[] = await getDisplayName(fids);
-
-  // Attach display names to sortedFids
-  for (let i = 0; i < sortedFids.length; i++) {
-    sortedFids[i].display_name = displayNames[i];
-  }
-
-  return sortedFids;
-}
-
-export async function weightedRaffle(fid: number): Promise<Fan | Participant> {
-  const fans = await searchMyFan(fid);
-  const totalWeight = fans.reduce((total, fan) => total + fan.score, 0);
-  let randomValue = Math.random() * totalWeight;
-
-  for (const fan of fans) {
-    randomValue -= fan.score;
-    if (randomValue <= 0) {
-      return fan;
-    }
-  }
-  // In case no winner was found (should not happen), return the last fan as a fallback
-  return fans[fans.length - 1];
 }
 
 export async function getUserPfpUrl(fid: number): Promise<string> {
@@ -148,7 +35,7 @@ export async function postLum0xTestFrameValidation(fid: number, path: string) {
     }),
   });
 }
-// farsightsummit채널에서 최근 캐스트 25개의 작성자중 한명 임의 선택 -> fid,display_name 반환
+// farsightsummit채널에서 최근 캐스트 25개의 작성자 중 한명 임의 선택 -> fid,display_name 반환
 export async function getUserFromChannel() {
   let res = await Lum0x.farcasterFeed.getFeed({
     feed_type: "filter",
